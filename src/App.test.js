261a0bba973bf14392685d63eb2e9fa82044ddb5
Ryan Drew <ryan.drew14@gmail.com>
2020-02-20 20:54:43 -0800

    Remove unnecessary create-react-app stuff

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Leaving this here because it's a great example of a basic test.
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
