b1a2d2a400dbec961ac793fda0242b49691c1795
Ryan Drew <ryan.drew14@gmail.com>
2020-02-23 21:34:39 -0800

    Add basic form to login page

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
 import BasicLayout from '../common/components/BasicLayout';
 
+const LoginForm = styled.form`
+  background-color: #fff;
+  padding: 30px 20px;
+  border: 1px solid #E5E4E2;
+  border-radius: 25px;
+`
+
+const FormInputWrapper = styled.div`
+  margin: 20px 10px 40px;
+`
+
+const FormInputTitle = styled.h3`
+  font-size: 14px;
+`
+
+const StyledFormInput = styled.input`
+  font-size: 18px;
+  padding: 10px 0 5px;
+  min-width: 100px;
+  border: none;
+  border-bottom: 1px solid #cdcdcd;
+`
+
+const LogInButton = styled.button`
+  margin: auto 0 auto auto;
+`
+
+const FormInput = ({ title, value, type, onChange }) => {
+  return (
+    <FormInputWrapper>
+    <FormInputTitle>{title}</FormInputTitle>
+    <StyledFormInput type={type} value={value} onChange={onChange}/>
+    </FormInputWrapper>
+  )
+}
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <BasicLayout title="Log In">
-      whatup this will be a form eventually
+      <LoginForm>
+        <FormInput title="Email" type="text" value={email} onChange={e => setEmail(e.target.value)}></FormInput>
+        <FormInput title="Password" type="password" value={password} onChange={e => setPassword(e.target.value)}></FormInput>
+        <LogInButton>Log In</LogInButton>
+      </LoginForm>
     </BasicLayout>
   );
 };
