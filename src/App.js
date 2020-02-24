909d897f686229fa68ee52979dbc1e644759df9b
Ryan Drew <ryan.drew14@gmail.com>
2020-02-23 19:05:56 -0800

    Bootstrap with basic libaries, add HomePage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import GlobalStyle, { CSSReset } from './GlobalStyle'
+import HomePage from './HomePage/HomePage';
 
 function App() {
   return (
-    <div>Hello world!</div>
+    <Router>
+      <CSSReset />
+      <GlobalStyle />
+      <Switch>
+        <Route path="/">
+          <HomePage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
