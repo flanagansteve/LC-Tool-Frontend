954dca727b21c5457115496544e60f52b2f913d4
Ryan Drew <ryan.drew14@gmail.com>
2020-03-31 21:43:44 -0400

    Add bank employee signup page

--- src/pages/bank/BankInvitePage.js
+++ src/pages/bank/BankInvitePage.js
@@ -73,7 +73,7 @@ const BankInvitePage = ({ history }) => {
         initialValues={{ email: '' }}
         onSubmit={(values, { setSubmitting }) => {
           setSubmitting(true);
-          makeAPIRequest(`/bank/${user.bank.id}/invite_teammate`, "POST", { "inviteeEmail": values.email }, true)
+          makeAPIRequest(`/bank/${user.bank.id}/invite_teammate/`, "POST", { "inviteeEmail": values.email }, true)
             .then((response) => {
               if (response.status === 200) setStatus({ 
                 status: "success",
