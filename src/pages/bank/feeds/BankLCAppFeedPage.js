7b684d277c6865735a4a340360c1943195832153
Ryan Drew <ryan.drew14@gmail.com>
2020-04-04 21:41:56 -0400

    Only show apps in LC apps page

--- src/pages/bank/feeds/BankLCAppFeedPage.js
+++ src/pages/bank/feeds/BankLCAppFeedPage.js
@@ -9,7 +9,7 @@ const BankLCAppFeedPage = () => {
   const bankid = get(user, ['bank', 'id']);
   return <LCFeed 
     title="All LC Applications 📝" 
-    url={`/lc/by_bank/${bankid}/`}
+    url={`/lc/by_bank/${bankid}/awaiting_issuer_approval/`}
     user={user}
     />
 }
