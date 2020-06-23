a51d0813b14df0ae31ea8090bf6146fd47ff0d81
Steve Flanagan <flanagan.s@husky.neu.edu>
2020-06-22 21:50:11 -0400

    fuck it push build to master

---
+++ build/precache-manifest.cc39b6d2190db1ea9257963e059224d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c298f1f9ef212a9866b9881c61383908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8d3c534914dfd88099a",
+    "url": "/static/js/2.0bb2508f.chunk.js"
+  },
+  {
+    "revision": "592908a17cc522aa1148a47c5cd62a22",
+    "url": "/static/js/2.0bb2508f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7639fed200a4ed55ad",
+    "url": "/static/js/main.5cc23de8.chunk.js"
+  },
+  {
+    "revision": "4a401bd74ebef46415bd",
+    "url": "/static/js/runtime-main.cca21cff.js"
+  },
+  {
+    "revision": "980300c8d42a3f0a1a87dfb66ec7d496",
+    "url": "/static/media/logo.980300c8.png"
+  }
+]);
