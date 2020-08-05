cf6e3c9a823766fc1eaf24b00069173156809af1
Steve Flanagan <flanagan.s@husky.neu.edu>
2020-08-04 22:33:35 -0400

    buildarino

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f46d60141657eb16daa3fa833136fbdc.js"
+  "/precache-manifest.8bf698ed3193a33546b364f8731715b7.js"
 );
 
 self.addEventListener('message', (event) => {
